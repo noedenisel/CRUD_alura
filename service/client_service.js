305a286b258c03a6ee12a659d78d9c5f5c72f492
NoeDenise <noelialombardo@hotmail.com>
2023-08-12 12:52:18 -0300

    actualizacion de packajes

--- service/client_service.js
+++ service/client_service.js
@@ -1,37 +1,5 @@
 console.log("client service conectado");
 
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr")
-    const contenido = ` 
-        <td class="td" data-td>${nombre}</td>
-        <td>${email}</td>
-        <td>
-            <ul class="table__button-control">
-                <li>
-                    <a
-                        href="../screens/editar_cliente.html"
-                        class="simple-button simple-button--edit"
-                    >Editar</a
-                    >
-                </li>
-                <li>
-                    <button
-                        class="simple-button simple-button--delete"
-                        type="button"
-                    >
-                        Eliminar
-                    </button>
-                </li>
-            </ul>
-        </td>
-    `
-  linea.innerHTML = contenido
-  return linea
-}
-
-const table = document.querySelector("[data-table]")
-
-
 const listaClientes = () =>
     fetch("http://localhost:3000/perfil")
         .then (respuesta =>
@@ -56,12 +24,6 @@ const listaClientes = () =>
     // })
     // return promise
 
-
-listaClientes().then((data)=>{
-    console.log(data);
-    data.forEach((perfil) => {
-    const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email)
-    table.appendChild(nuevaLinea)
-})
-}).catch((error) => alert(error))
-
+    export const clienteServices = {
+        listaClientes
+    }
